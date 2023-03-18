535121fe242d8c89f5821f2476e589ceb94a6ce7
LazyBrush <andy@winton.net>
2023-03-18 23:20:12 +0000

    feat(dockerfiles): now with react and nodejs based dockerfiles

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -2,6 +2,7 @@
 
 echo "Shell script to build image"
 APP=$1
+TYPE=$2
 TAG=$(jq -S -r '.version' package.json)
 
 if [ "$APP" == "" ]
@@ -10,6 +11,12 @@ then
   exit 1
 fi
 
+if [ "$TYPE" != "nodejs" ] && [ "$TYPE" != "react" ]
+then
+  echo "Error: only 'nodejs' and 'react' TYPE Dockerfiles supported"
+  exit 1
+fi
+
 echo Running:
-echo docker build --build-arg APP="$APP" -t "$APP":"$TAG" .
-docker build --build-arg APP="$APP" -t "$APP":"$TAG" .
+echo docker build -f dockerfiles/Dockerfile."$TYPE" --build-arg APP="$APP" -t "$APP":"$TAG" .
+docker build -f dockerfiles/Dockerfile."$TYPE" --build-arg APP="$APP" -t "$APP":"$TAG" .

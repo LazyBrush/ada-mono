87dad8264bbcbccca126763d5204723bd8ca857e
LazyBrush <andy@winton.net>
2023-03-19 22:32:59 +0000

    feat(catface-next): added catface-next a nextjs app

---
+++ apps/catface-next/jest.config.ts
@@ -0,0 +1,19 @@
+/* eslint-disable */
+export default {
+  displayName: 'catface-next',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+  coverageDirectory: '../../coverage/apps/catface-next',
+};

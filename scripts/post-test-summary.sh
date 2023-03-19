ddbca1dc9e346bd919c29110b868b2d66b282bf1
LazyBrush <andy@winton.net>
2023-03-19 16:01:02 +0000

    feat(test): tidied up test summary

--- scripts/post-test-summary.sh
+++ scripts/post-test-summary.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+echo
 echo Coverage Summary of all tests
+echo
 
 SUMMARY_DIR=coverage/post-test-summary
 mkdir -p $SUMMARY_DIR
-npx istanbul-merge --out $SUMMARY_DIR/coverage.json $(find coverage -name coverage-final.json)
+yarn -s istanbul-merge --out $SUMMARY_DIR/coverage.json $(find coverage -name coverage-final.json)
 
-npx istanbul report --include $SUMMARY_DIR/coverage.json text
+yarn -s istanbul report --include $SUMMARY_DIR/coverage.json text
 
-npx istanbul report --include $SUMMARY_DIR/coverage.json text-summary
+yarn -s istanbul report --include $SUMMARY_DIR/coverage.json text-summary
